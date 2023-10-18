3fc4bc02995289faac009de7ad65316bb4acc74e
master <sheriffsarafa@gmail.com>
2023-10-18 01:55:40 -0700

    edit 0n 00 pF 02n

--- csprintf.c
+++ csprintf.c
@@ -9,7 +9,8 @@
 int _printf(const char *format, ...)
 {
 	va_list arg_list;
-	int char_len = 0;
+	int char_len = 0, ch, s_len;
+	char *str;
 
 	if (format == NULL)
 		return (-1);
@@ -26,7 +27,30 @@ int _printf(const char *format, ...)
 			else
 			{
 				format++;
-				_chckarg(&*format, char_len, arg_list);
+				if (*format == '%')
+				{
+					write(1, format, 1);
+					char_len++;
+				}
+				else if (*format == 'c')
+				{
+					ch = va_arg(arg_list, int);
+					write(1, &ch, 1);
+					char_len++;
+				}
+				else if (*format == 's')
+				{
+					str = va_arg(arg_list, char *);
+					s_len = _strlen(str);
+					write(1, str, s_len);
+					char_len += s_len;
+				}
+				else
+				{
+					format--;
+					write(1, format, 1);
+					char_len++;
+				}
 			}
 			format++;
 		}
